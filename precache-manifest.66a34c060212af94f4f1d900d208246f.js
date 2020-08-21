44d9145cd9ff5ebd42e424e95faf2af183049a01
EfraimKrug <efraimmkrug@gmail.com>
2020-08-21 12:35:35 -0400

    Updates

---
+++ precache-manifest.66a34c060212af94f4f1d900d208246f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b58a4aa96144845e7a246694fcece1",
+    "url": "/nehiezra/index.html"
+  },
+  {
+    "revision": "d57d788ef7b77c1c8c9d",
+    "url": "/nehiezra/static/css/main.7e3a98ef.chunk.css"
+  },
+  {
+    "revision": "b65c6f93f3ff32f584af",
+    "url": "/nehiezra/static/js/2.84242264.chunk.js"
+  },
+  {
+    "revision": "e7db9ac6512e7fc159d3633db8f2050e",
+    "url": "/nehiezra/static/js/2.84242264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d57d788ef7b77c1c8c9d",
+    "url": "/nehiezra/static/js/main.42ca64f9.chunk.js"
+  },
+  {
+    "revision": "8a3ce843734654893e37",
+    "url": "/nehiezra/static/js/runtime-main.a398892f.js"
+  },
+  {
+    "revision": "ab4de693220c5bb736b6f8b10f7b9390",
+    "url": "/nehiezra/static/media/BeRonYahad.ab4de693.jpeg"
+  },
+  {
+    "revision": "3bd259c316da7c57e6451741419d05a3",
+    "url": "/nehiezra/static/media/LogoKorenComingSoon.3bd259c3.jpg"
+  },
+  {
+    "revision": "9a48f816853d9aa15eb61dbc2c202870",
+    "url": "/nehiezra/static/media/Nehemia_Polen_In_Search_of_the_Broken_Se.9a48f816.pdf"
+  },
+  {
+    "revision": "838000280b04a942018f21a11cfd8baa",
+    "url": "/nehiezra/static/media/ShlomoSemiStam.83800028.ttf"
+  },
+  {
+    "revision": "06c0e18d056901b4dd9040da9a586154",
+    "url": "/nehiezra/static/media/TheHolyFire.06c0e18d.jpg"
+  },
+  {
+    "revision": "487796cd4303d2cb98e8e6ff20fb0ff0",
+    "url": "/nehiezra/static/media/TheRebbesDaughter.487796cd.jpg"
+  },
+  {
+    "revision": "c87f8f5798729dd11ecdc803b96df194",
+    "url": "/nehiezra/static/media/TiberiasWithLove.c87f8f57.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nehiezra/static/media/logo.5d5d9eef.svg"
+  }
+]);
